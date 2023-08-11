cf88793af58fee21f0929eeb6f9153e6ffa7f6a1
Farrhouq <farouqimoro@gmail.com>
2023-08-11 22:58:03 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,35 +19,31 @@ int main(void)
 	{
 		for (j = 48; j < 58; j++)
 		{
-
 			next_k = i;
 			next_l = j + 1;
-			if (j == 9)
+			if (j == 57)
 			{
 				next_k = i + 1;
 				next_l = 48;
 			}
-
-			for (k = next_k; k < 58; k++)
+			for (k = i; k < 58; k++)
 			{
-				for (l = next_l; l < 58; l++)
+				for (l = j; l < 58; l++)
 				{
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(l);
-
+				if (!(i == 48 && j == 48 && k == 48 && l == 48))
+				{
+				putchar(i);
+				putchar(j);
+				putchar(32);
+				putchar(k);
+				putchar(l);
 					if (!(i == 57 && j == 56 && k == 57 && l == 57))
 					{
 						putchar(44);
 						putchar(32);
 					}
-				}
-			}
-		}
+				}}}}
 	}
-
 	putchar(10);
 	return (0);
 }
