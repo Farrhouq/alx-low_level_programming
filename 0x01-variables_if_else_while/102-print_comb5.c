97344e5437cf7297f0ae780315d849524ad859ae
Farrhouq <farouqimoro@gmail.com>
2023-08-11 23:31:57 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -12,37 +13,37 @@ int main(void)
 	int j;
 	int k;
 	int l;
-	int next_k;
-	int next_l;
+	int stop;
 
+	stop = 0;
 	for (i = 48; i < 58; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			next_k = i;
-			next_l = j + 1;
-			if (j == 57)
-			{
-				next_k = i + 1;
-				next_l = 48;
-			}
+			if (stop)
+				break;
 			for (k = i; k < 58; k++)
 			{
 				for (l = j; l < 58; l++)
 				{
 				if (!(i == 48 && j == 48 && k == 48 && l == 48))
 				{
+				if (i == 57 && j == 57 && k == 57 && l == 57)
+				{
+					stop = 1;
+					break;
+				}
 				putchar(i);
 				putchar(j);
 				putchar(32);
 				putchar(k);
 				putchar(l);
-					if (!(i == 57 && j == 56 && k == 57 && l == 57))
-					{
-						putchar(44);
-						putchar(32);
-					}
-				}}}}
+				if (!(i == 57 && j == 56 && k == 57 && l == 57))
+				{
+					putchar(44);
+					putchar(32);
+				}}
+				}}}
 	}
 	putchar(10);
 	return (0);
