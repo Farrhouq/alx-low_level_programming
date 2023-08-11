c49d8d6f93f465bb1656cb3ee9f36047201a5544
Farrhouq <farouqimoro@gmail.com>
2023-08-11 10:46:45 +0000

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - prints the lowercase alphabet in
+ * reverse
+ * Return: always 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
