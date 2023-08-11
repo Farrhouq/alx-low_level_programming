a9bb3e824283846b15ee4c6a47dced82228824be
Farrhouq <farouqimoro@gmail.com>
2023-08-11 10:34:38 +0000

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the
+ * lowercase alphabet, and then the uppercase alphabet
+ * Return: always 0
+*/
+int main(void)
+{
+	puts("0123456789");
+	return (0);
+}
