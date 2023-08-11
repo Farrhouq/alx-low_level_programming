e7323cd427bbb0bf1b9ceb4f7b802269b3d95252
Farrhouq <farouqimoro@gmail.com>
2023-08-11 10:16:58 +0000

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
+/**
+ * main - prints out the
+ * lowercase alphabet
+ * Return: returns the exit code
+*/
+int main(void)
+{
+	int i;
 
+	for (i = 97; i < 123; i++)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+	return (0);
 }
