9c1e16d39208c33eefdffc0e243e78a394f01081
Farrhouq <farouqimoro@gmail.com>
2023-08-20 00:09:14 +0000

    update

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the first 100 fibonacci numbers
+ * Return: 0 always
+*/
+int main(void)
+{
+	long int prev1, prev2, next;
+	int i;
+
+	prev1 = 1;
+	prev2 = 2;
+
+	printf("%ld, %ld, ", prev1, prev2);
+	for (i = 0; i < 98; i++)
+	{
+		next = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = next;
+		printf("%ld", next);
+		if (i != 47)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
+}
