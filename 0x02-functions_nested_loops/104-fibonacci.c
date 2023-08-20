69f53613cbc9958ccc65f164c5b954f5b9fbe33e
Farrhouq <farouqimoro@gmail.com>
2023-08-20 00:52:11 +0000

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	prev2 = 2;
 
 	printf("%ld, %ld, ", prev1, prev2);
-	for (i = 0; i < 83; i++)
+	for (i = 0; i < 86; i++)
 	{
 		next = prev1 + prev2;
 		prev1 = prev2;
 		prev2 = next;
 		printf("%ld", next);
-		if (i != 83)
+		if (i != 86)
 			printf(", ");
 	}
 	putchar('\n');
