26257b32a64e1b04b1d75c4d0dc7f18efe803250
Farrhouq <farouqimoro@gmail.com>
2023-08-29 10:18:52 +0000

    update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: the string
+ * Return: the encoded string
+ */
+char *rot13(char *s)
+{
+	int i, mag, j;
+	char t[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int m[] = {97, 65};
+
+	i = 0;
+	while (s[i])
+	{
+		if ((s[i] >= 97 && s[i] <= 122) || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			for (j = 0; j < 52; j++)
+			{
+				if (s[i] == t[j])
+				{
+					mag = m[(j + 1) / 46];
+					break;
+				}
+			}
+
+			s[i] = ((s[i] + 13 - mag) % 26) + mag;
+		}
+		i++;
+	}
+	return (s);
+}
