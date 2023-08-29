7b3ce472ab61482278ff0f03823de0f52ef39bab
Farrhouq <farouqimoro@gmail.com>
2023-08-29 09:08:53 +0000

    update

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,14 +9,14 @@ char *leet(char *s)
 {
 	int i, j;
 	char special[] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'};
-	char leets[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char leets[] =   {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	i = 0;
 	while (s[i])
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (s[i] == special[j] && (s[i] >= 97 && s[i] <= 122))
+			if (s[i] == special[j])
 			{
 				s[i] = leets[j];
 				break;
