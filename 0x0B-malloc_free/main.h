afaf4ca34a34d25c29091813462ec261c7c1c17d
Farrhouq <farouqimoro@gmail.com>
2023-09-05 15:32:02 +0000

    update

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -11,3 +11,7 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 
 char **strtow(char *str);
+
+int count_words(char *str);
+
+char *strdup_range(char *start, char *end);
