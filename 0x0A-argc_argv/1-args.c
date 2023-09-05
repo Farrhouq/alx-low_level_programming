be3a7a96b11694baf2ab73dd066dc04282c7414d
Farrhouq <farouqimoro@gmail.com>
2023-09-05 07:16:10 +0000

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point.
+ * Description- prints the number
+ * of arguments passed to it
+ * @argc: the number of arguments
+ * @argv: the argument list
+ * Return: 0 always
+*/
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
