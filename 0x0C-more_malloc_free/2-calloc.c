c8eefc6307cebac11b3113fa80cbca2080b297f5
Farrhouq <farouqimoro@gmail.com>
2023-09-08 00:28:59 +0000

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
+	int i, *arrayn;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	array = malloc(size * nmemb);
+	arrayn = (int *)array;
 	if (array == NULL)
 		return (NULL);
+	
+	for (i = 0; i < size; i++)
+		arrayn[i] = 0;
+
 	return (array);
 }
