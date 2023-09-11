47a1575b77761bfae08679ab2b822a8b6a5416df
Farrhouq <farouqimoro@gmail.com>
2023-09-11 10:07:33 +0000

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - like a normal dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: who owns the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
