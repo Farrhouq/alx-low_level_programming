6b46f3fede1229cec73791c41bd7b3584fc561af
Farrhouq <farouqimoro@gmail.com>
2023-09-11 15:42:32 +0000

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,49 +2,73 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
- * Return: a pointer to the new dog
+ * _strcpy - copies a string to another
+ * @dest: string to receive copied text
+ * @src: source of copied text
+ *
+ * Return: copied text in the form of dest
  */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int j, len;
+
+	for (len = 0; src[len] != '\0'; len++)
+		;
+
+	for (j = 0; j <= len; j++)
+	{
+		dest[j] = src[j];
+	}
+
+	return ((dest));
+}
+
+/**
+ * new_dog - creates a new dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: newly created dog
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ndog;
-	int i;
+	dog_t *new_d;
+	char *name_cpy, *owner_cpy;
+	int len1 = 0, len2 = 0;
 
-	ndog = malloc(sizeof(dog_t));
-	if (ndog == NULL)
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
 		return ((NULL));
-	ndog->name = malloc(sizeof(name));
-	if (ndog->name == NULL)
-	{
-		free(ndog);
-		return (NULL);
-	}
-	ndog->owner = malloc(sizeof(owner));
-	if (ndog->owner == NULL)
-	{
-		free(ndog);
-		return (NULL);
-	}
 
-	i = 0;
-	while (name[i] != '\0')
-	{
-		ndog->name[i] = name[i];
-		i++;
-	}
-	ndog->name[i] = '\0';
+	while (name[len1])
+		len1++;
+	while (owner[len2])
+		len2++;
 
-	i = 0;
-	while (owner[i] != '\0')
+	name_cpy = malloc(sizeof(char) * (len1 + 1));
+	if (name_cpy == NULL)
 	{
-		i++;
-		ndog->owner[i] = owner[i];
+		free(new_d);
+		return ((NULL));
 	}
-	ndog->owner[i] = '\0';
+	_strcpy(name_cpy, name);
 
-	ndog->age = age;
-	return (ndog);
+	owner_cpy = malloc(sizeof(char) * (len2 + 1));
+	if (owner_cpy == NULL)
+	{
+		free(name_cpy);
+		free(new_d);
+		return ((NULL));
+	}
+	_strcpy(owner_cpy, owner);
+
+	new_d->name = name_cpy;
+	new_d->owner = owner_cpy;
+	new_d->age = age;
+
+	return ((new_d));
 }
