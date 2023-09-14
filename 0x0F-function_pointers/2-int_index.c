cabdaabe9cbd2f0c25bd05e1fefd71ca5e667d27
Farrhouq <farouqimoro@gmail.com>
2023-09-14 10:26:54 +0000

    update

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,12 +16,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 
 	if (cmp == NULL || array == NULL)
-		return;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (array[i]);
+			return (i);
 	}
 	return (-1);
 }
