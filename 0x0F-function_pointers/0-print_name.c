633b876c668c8f288832fc41a0fc46f48d89a348
Farrhouq <farouqimoro@gmail.com>
2023-09-14 10:06:41 +0000

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -7,5 +8,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
+    if (f == NULL)
+        return;
 	f(name);
 }
