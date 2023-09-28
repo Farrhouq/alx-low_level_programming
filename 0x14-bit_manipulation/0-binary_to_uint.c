88333d53bc00f6df9c944becc4282f20a4776b21
Farrhouq <farouqimoro@gmail.com>
2023-09-28 15:46:04 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,34 +7,34 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i, j, pv = 1, len = 0;
-    unsigned int value = 0;
+	int i, j, pv = 1, len = 0;
+	unsigned int value = 0;
 
-    if (b == NULL)
-        return (0);
+	if (b == NULL)
+		return (0);
 
-    for (i = 0; b[i]; i++)
-        len++;
+	for (i = 0; b[i]; i++)
+		len++;
 
-    for (i = len - 1; i >= 0; i--)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
-        if (b[i] == '1')
-        {
-            pv = 1;
-            if (i == (len - 1))
-            {
-                pv = 1;
-            }
-            else
-            {
-                for (j = 0; j < (len - i - 1); j++)
-                    pv *= 2;
-            }
-            value += pv;
-        }
-    }
-    return (value);
+		if (b[i] == '1')
+		{
+			pv = 1;
+			if (i == (len - 1))
+			{
+				pv = 1;
+			}
+			else
+			{
+				for (j = 0; j < (len - i - 1); j++)
+					pv *= 2;
+			}
+			value += pv;
+		}
+	}
+	return (value);
 }
