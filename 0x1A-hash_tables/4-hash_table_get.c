57edc78c7844c1f4490c93455f97c5e783dc9a36
Farrhouq <farouqimoro@gmail.com>
2024-02-14 12:44:06 +0000

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value from a hash table
+ * @ht: the hash table
+ * @key: the key
+ *
+ * Return: the value at the key
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long index;
+	hash_node_t *head;
+
+	index = key_index((unsigned char *)key, ht->size);
+	head = ht->array[index];
+	while (head != NULL)
+	{
+		if (strcmp(head->key, key) == 0)
+			return (head->value);
+
+		head = head->next;
+	}
+	return (NULL);
+}
