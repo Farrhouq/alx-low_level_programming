7087956f956d59fc2a20f3a7c155e0b26f1bac56
Farrhouq <farouqimoro@gmail.com>
2024-02-14 02:11:42 +0000

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,8 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return (1);
 
-	if (key == NULL || *key == '\0')
-		return (0);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
