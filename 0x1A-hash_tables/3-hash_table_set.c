59c164445a2943c389ff1645a76c0227dad38550
Farrhouq <farouqimoro@gmail.com>
2024-02-14 12:22:17 +0000

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,31 +1,39 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - adds a key-value pair to a hash table
+ * @ht: the hash table
+ * @key: the new key
+ * @value: the value of the new key
+ *
+ * Return: 1 on success, 9 otherwise
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long index;
-    hash_node_t *node, *old_node;
-    char *val = strdup(value);
+	unsigned long index;
+	hash_node_t *node, *old_node;
+	char *val = strdup(value);
 
-    node = malloc(sizeof(hash_node_t));
-    if (node == NULL)
-        return (0);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
 
-    index = key_index((unsigned char*)key, ht->size);
-    node->key = (char *)key;
-    node->next = NULL;
-    strcpy(val, value);
-    node->value = val;
+	index = key_index((unsigned char *)key, ht->size);
+	node->key = (char *)key;
+	node->next = NULL;
+	strcpy(val, value);
+	node->value = val;
 
-    if (ht->array[index] != NULL)
-    {
-        old_node = ht->array[index];
-        ht->array[index] = node;
-        node->next = old_node;
-    }
-    else
-    {
-        ht->array[index] = node;
-    }
+	if (ht->array[index] != NULL)
+	{
+		old_node = ht->array[index];
+		ht->array[index] = node;
+		node->next = old_node;
+	}
+	else
+	{
+		ht->array[index] = node;
+	}
 
-    return (1);
+	return (1);
 }
