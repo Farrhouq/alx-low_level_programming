0197ddfdebf249e9e18f47a9d60a20b181c724ad
Farrhouq <farouqimoro@gmail.com>
2024-02-14 01:44:14 +0000

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	hash_node_t **array;
 
-	if (size < 0)
-		return (NULL);
-
 	new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
@@ -25,6 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (array == NULL)
 	{
+		free(new_table);
 		return (NULL);
 	}
 
