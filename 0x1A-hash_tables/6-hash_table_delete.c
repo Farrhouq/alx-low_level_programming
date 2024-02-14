d0e6deea66e43f4f0e6addbe81cdd50b23635571
Farrhouq <farouqimoro@gmail.com>
2024-02-14 13:34:21 +0000

    update

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - frees a hash table
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				hash_node_t *temp = node;
+
+				node = node->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
