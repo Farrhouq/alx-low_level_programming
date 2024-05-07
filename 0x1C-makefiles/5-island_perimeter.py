89c5f7dafbe71ab596a65ec755267f6153bb613c
Farrhouq <farouqimoro@gmail.com>
2024-05-07 14:39:54 +0000

    update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module contains the island_perimeter function"""
+
+
 def island_perimeter(grid):
+    """ that returns the perimeter of the island described in grid"""
     total_perimeter = 0
     old_land_indices = []
     for row in grid:
@@ -9,12 +13,13 @@ def island_perimeter(grid):
             if row[cell]:
                 land_count += 1
                 new_land_indices.append(cell)
-        
+
         row_perimeter = land_count*2 + 2 if land_count else 0
-        joining_points = [j for j in range(len(row)) if j in new_land_indices and j in old_land_indices]
+        joining_points = [j for j in range(
+            len(row)) if j in new_land_indices and j in old_land_indices]
         row_perimeter -= 2*len(joining_points)
 
-        total_perimeter += row_perimeter    
+        total_perimeter += row_perimeter
         old_land_indices = new_land_indices
 
     return total_perimeter
